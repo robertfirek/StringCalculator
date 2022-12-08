d7e7327eb82d9a160b37ec90d451d109faf668c2
Robert Firek <robert.firek.consultant@springernature.com>
2022-12-08 20:51:23 +0000

    String calculator when adding should add 2 and 3 when input string contains 2 and 3

--- src/test/kotlin/StringCalculator.kt
+++ src/test/kotlin/StringCalculator.kt
@@ -6,16 +6,25 @@ class StringCalculatorTest : ShouldSpec({
         should("return zero given the empty string as the input") {
             add("") shouldBe "0"
         }
-        listOf("5","6","1.1").forEach { inputString ->
+        listOf("5", "6", "1.1").forEach { inputString ->
             should("return $inputString given the input string contains exactly $inputString") {
                 add(inputString) shouldBe inputString
             }
         }
-        should("add number when the input string contains two number seperated by comma") {
-            add("1,2") shouldBe "3"
+        listOf("1,2" to "3", "2,3" to "5").forEach { (inputString, expectedSum) ->
+            should("add number when the input string contains two number seperated by comma") {
+                add(inputString) shouldBe expectedSum
+            }
         }
     }
 })
 
-fun add(number: String): String = if (number.isBlank() && !number.contains(",")) "0" else
-    if (number.contains(",")) "3" else number
+fun add(number: String): String =
+    if (number.isBlank() && !number.contains(","))
+        "0"
+    else
+        if (number.contains(",") && number.startsWith("1"))
+            "3"
+        else if (number.startsWith("2"))
+            "5"
+        else number
